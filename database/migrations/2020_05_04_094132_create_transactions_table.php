08e8e724995ff2d3410928534b372b95e046e0aa
gafriputra <gafri.putra@gmail.com>
2020-05-04 16:52:42 +0700

    Create Table Transaction

---
+++ database/migrations/2020_05_04_094132_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice');
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->string('phone', 20);
+            $table->string('address');
+            $table->bigInteger('transaction_total');
+            $table->string('status', 20);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
