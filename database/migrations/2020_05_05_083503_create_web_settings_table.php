53c78fa0e079db7f7b9c80cd279c62fd1b887057
gafriputra <gafri.putra@gmail.com>
2020-05-05 15:40:41 +0700

    create web setting table

---
+++ database/migrations/2020_05_05_083503_create_web_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('web_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('slogan');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('image');
+            $table->text('description');
+            $table->string('address');
+            $table->string('store_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('web_settings');
+    }
+}
