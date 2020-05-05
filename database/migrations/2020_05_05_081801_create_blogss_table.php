d315d8bf23d6ad9f2341e7e4720e90fb1cfdf0e1
gafriputra <gafri.putra@gmail.com>
2020-05-05 15:28:05 +0700

    create blog table

---
+++ database/migrations/2020_05_05_081801_create_blogss_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogssTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('blog_category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('image');
+            $table->text('content');
+            $table->integer('author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
