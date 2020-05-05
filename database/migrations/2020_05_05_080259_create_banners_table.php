79989ce21b0d0b827e363f1e9c94f14a03e48263
gafriputra <gafri.putra@gmail.com>
2020-05-05 15:13:01 +0700

    create banner and teams table

---
+++ database/migrations/2020_05_05_080259_create_banners_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('header1', 20);
+            $table->string('header2', 20);
+            $table->string('caption', 50);
+            $table->string('link');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
